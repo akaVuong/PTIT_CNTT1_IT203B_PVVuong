0a0513161fbf119fd54ad50d70731ac8d0c1b815
vuong2k6ss <vuongvanphung2k6@gmail.com>
2026-03-24 22:05:31 +0700

    up

---
+++ SS13/src/main/java/Bai4/DichVu.java
@@ -0,0 +1,11 @@
+package Bai4;
+
+public class DichVu {
+    private int maDichVu;
+    private String tenDichVu;
+
+    public DichVu(int maDichVu, String tenDichVu) {
+        this.maDichVu = maDichVu;
+        this.tenDichVu = tenDichVu;
+    }
+}
---
+++ SS13/src/main/java/Bai4/Main.java
@@ -0,0 +1,14 @@
+package Bai4;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        HospitalDAO dao = new HospitalDAO();
+        List<BenhNhanDTO> list = dao.getAllBenhNhan();
+        for (BenhNhanDTO bn : list) {
+            System.out.println("BN: " + bn);
+            for (DichVu dv : bn.getDsDichVu()) {
+                System.out.println("   DV: " + dv);
+            }
+        }
+    }
+}
